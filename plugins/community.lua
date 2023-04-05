e412f5016580857c606759275a6875d58e9c249c
joao.rosa <joao.rosa@idwall.co>
2023-04-05 18:55:39 -0300

    config

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,8 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.java" },
 }
